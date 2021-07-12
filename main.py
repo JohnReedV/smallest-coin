75e52f09e0b6edcb7d3a53b4543462907534d480
jjreedv <87283488+jjreedv@users.noreply.github.com>
2021-07-12 13:58:18 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+input_string = input("Enter three numbers: ")
+coins = input_string.split()
+coins = list(map(int, coins))
+coins.sort(reverse=True)
+data = int(input('''Enter a number, I will find the minimum amount of the previously entered values it will take in order to add them to this number: '''))
+final = 0
+add = 0
+
+for number in coins:
+    while final <= data:
+        if final + number <= data:
+            final = final + number
+            add = add + 1
+        else:
+            break
+    while final <= data:
+        if final + number <= data:
+            final = final + number
+            add = add + 1
+        else:
+            break
+    while final <= data:
+        if final + number <= data:
+            final = final + number
+            add = add + 1
+        else:
+            break
+print(add)
